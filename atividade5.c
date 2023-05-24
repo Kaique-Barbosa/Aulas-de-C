27bb11b8b6adbc26ec8b523e5a25061d5663e5e0
Kaique Barbosa <88518303+Kaique-Barbosa@users.noreply.github.com>
2023-05-24 14:04:58 -0300

    atividade5.c

--- atividade5.c
+++ atividade5.c
@@ -1,54 +1,55 @@
 
-//5. Elabore um algoritmo para receber dois inteiros como entrada do teclado e escreva na tela: 
-//A média, a soma, o produto, o menor valor e o maior valor. 
-//Além disso, verifique se os números informados pelo usuário são iguais. 
-//Usando uma linha para cada resultado. 
+// 5. Elabore um algoritmo para receber dois inteiros como entrada do teclado e escreva na tela:
+// A média, a soma, o produto, o menor valor e o maior valor.
+// Além disso, verifique se os números informados pelo usuário são iguais.
+// Usando uma linha para cada resultado.
 
 #include <stdio.h>
 #include <stdlib.h>
 
+void main()
+{
 
-int main(){
+    int i;
+    int maiorValor, menorValor, soma, media, produto;
+    int valor[2];
 
-int i;
-int  maiorValor,menorValor,soma, media, produto; 
-int valor[2];
-
-for (i=1; i <=2; i++){
-printf("Digite um valor inteiro\n");
-scanf("%d", &valor[i]);
-}
-
-system("cls");
-
-
-for (i=1; i <=2; i++){
-    soma = soma + valor[i];
-    if (valor[i] > maiorValor){
-        maiorValor = valor[i];
-    }else{
-        menorValor = valor[i];
+    for (i = 0; i < 2; i++)
+    {
+        printf("Digite um valor inteiro\n");
+        scanf("%d", &valor[i]);
     }
-}
 
-media = (soma / 2);
-produto = (valor[1] * valor[2]);
+    system("cls");
 
-// MOSTRANDO RESULTADO
-printf("Media: %.1d\n",media);
-printf("Soma: %.1d\n",soma);
-printf("Produto: %.1d\n", produto);
-printf("Maior Valor: %.1d\n",maiorValor);
-printf("Menor Valor: %.1d\n",menorValor);
+    for (i = 0; i < 2; i++)
+    {
+        soma = soma + valor[i];
+        if (valor[i] > maiorValor)
+        {
+            maiorValor = valor[i];
+        }
+        else
+        {
+            menorValor = valor[i];
+        }
+    }
 
+    media = (soma / 2);
+    produto = (valor[0] * valor[1]);
 
-// Verificar se os valores são iguais
-if(valor[1] == valor[2]){
-  printf("Os valores digitados sao iguais");  
-} else{
-  printf("Os valores digitados sao difetentes");    
-}
+    // MOSTRANDO RESULTADO
+    printf("Media: %.1d\n", media);
+    printf("Soma: %.1d\n", soma);
+    printf("Produto: %.1d\n", produto);
 
-
-    return 0;
+    // Verificar se os valores são iguais
+    if (valor[0] == valor[1]){
+        printf("Os valores digitados sao iguais");
+    }
+    else
+    {
+        printf("Maior Valor: %.1d\n", maiorValor);
+        printf("Menor Valor: %.1d\n", menorValor);
+    }
 }
