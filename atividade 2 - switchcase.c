e96879044dfc3b4333ddfc19988f18b7a68207ec
Kaique Barbosa <88518303+Kaique-Barbosa@users.noreply.github.com>
2023-06-13 16:53:48 -0300

    Add files via upload

--- atividade 2 - switchcase.c
+++ atividade 2 - switchcase.c
@@ -17,6 +17,55 @@ Desenvolva o código que será utilizado para a realização dos testes com os t
 selecionados.
 */
 
-void main(){
 
+
+void main(){
+ int i;
+ int codigoDigitado;
+char produto[3][250];
+int codigo[3];
+float valor[3];
+
+
+//for ( i = 0; i < 3; i++){
+    printf("dgite o codigo do produto");
+    scanf("%d", &codigoDigitado);
+//}
+  // banco de dados//
+produto[0] = "detergente";
+codigo[0] = 100; //codigo do produto
+valor[0] = 1.59; //valor unitario
+
+produto[1] = "esponja";
+codigo[1] = 101; //codigo do produto
+valor[1] = 4.59;//valor unitario
+
+produto[2] = "la de aco";
+codigo[2] = 102;//codigo do produto
+valor[2] = 1.79;//valor unitario
+
+  //fim banco de dados  
+
+for ( i = 0; i < 3; i++){
+
+switch (codigoDigitado){
+
+case  100:
+    printf("%s", produto[0]);
+    break;
+
+case 101:
+    printf("%s", produto[1]);
+    break;
+
+case 102:
+    printf("%s", produto[2]);
+    break;   
+
+default:
+    break;
+}
+
+}
+    
 }
